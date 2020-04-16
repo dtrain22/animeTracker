7f0d543adb0f5afc3bc4317d9a31ae5ee184bb66
Damian Metz <DRMetz01@indianatech.net>
2020-04-16 18:28:23 -0400

    #12 Have Login redirect to MainActivity
    After logging in login should redirect to main activity

--- app/src/main/java/com/example/animetracker/data/network/repositories/AccessTokenRepository.kt
+++ app/src/main/java/com/example/animetracker/data/network/repositories/AccessTokenRepository.kt
@@ -1,6 +1,7 @@
 package com.example.animetracker.data.network.repositories
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import com.example.animetracker.BuildConfig
 import com.example.animetracker.data.AccessTokenObject
 import okhttp3.*
@@ -12,41 +13,47 @@ class AccessTokenRepository(
     val accessTokenObject:AccessTokenObject = AccessTokenObject
 ) {
 
-    fun getAccessToken(code:String) {
-        try {
-            val client = OkHttpClient.Builder()
-                .connectTimeout(10, TimeUnit.SECONDS)
-                .writeTimeout(10, TimeUnit.SECONDS)
-                .readTimeout(10, TimeUnit.SECONDS)
-                .build()
+    fun getAccessToken(code:String, _accessToken:MutableLiveData<String>) {
+        if(accessTokenObject.accessToken != null) {
+            _accessToken.postValue(accessTokenObject.accessToken)
+        } else {
+            try {
+                val client = OkHttpClient.Builder()
+                    .connectTimeout(10, TimeUnit.SECONDS)
+                    .writeTimeout(10, TimeUnit.SECONDS)
+                    .readTimeout(10, TimeUnit.SECONDS)
+                    .build()
 
-            val formBody = FormBody.Builder()
-                .add("grant_type", "authorization_code")
-                .add("client_id", BuildConfig.CLIENT_ID)
-                .add("client_secret", BuildConfig.CLIENT_SECRET)
-                .add("redirect_uri", BuildConfig.REDIRECT_URI)
-                .add("code",code)
-                .build()
+                val formBody = FormBody.Builder()
+                    .add("grant_type", "authorization_code")
+                    .add("client_id", BuildConfig.CLIENT_ID)
+                    .add("client_secret", BuildConfig.CLIENT_SECRET)
+                    .add("redirect_uri", BuildConfig.REDIRECT_URI)
+                    .add("code",code)
+                    .build()
 
-            val request = Request.Builder()
-                .url("https://anilist.co/api/v2/oauth/token")
-                .post(formBody)
-                .build()
+                val request = Request.Builder()
+                    .url("https://anilist.co/api/v2/oauth/token")
+                    .post(formBody)
+                    .build()
 
-            client.newCall(request).enqueue(object : Callback {
-                override fun onFailure(call: Call, e: IOException) {
-                    Log.e("Error","failure")
-                }
+                client.newCall(request).enqueue(object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        Log.e("Error","failure")
+                    }
 
-                override fun onResponse(call: Call, response: Response) {
-                    val jsonData: String = response.body()!!.string()
-                    val jsonObject = JSONObject(jsonData)
+                    override fun onResponse(call: Call, response: Response) {
+                        val jsonData: String = response.body()!!.string()
+                        val jsonObject = JSONObject(jsonData)
 
-                    accessTokenObject.accessToken = jsonObject.getString("access_token")
-                }
-            })
-        }  catch (e: IOException) {
-            e.printStackTrace()
+                        accessTokenObject.accessToken = jsonObject.getString("access_token")
+                        _accessToken.postValue(accessTokenObject.accessToken)
+                    }
+                })
+            }  catch (e: IOException) {
+                e.printStackTrace()
+            }
         }
+
     }
 }
--- app/src/main/java/com/example/animetracker/ui/MainActivity.kt
+++ app/src/main/java/com/example/animetracker/ui/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.animetracker.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.animetracker.R
+import com.example.animetracker.data.AccessTokenObject
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
